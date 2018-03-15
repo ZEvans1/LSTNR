05d96531cd2eeda0e3f32959c57b5b8768ce7f80
Student <example@example.com>
2018-03-15 15:08:30 -0700

    Create form and button for displaying one number on the travel activity.

--- app/src/main/java/com/example/guest/lstnr/TravelActivity.java
+++ app/src/main/java/com/example/guest/lstnr/TravelActivity.java
@@ -1,9 +1,12 @@
 package com.example.guest.lstnr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -11,7 +14,8 @@ import butterknife.ButterKnife;
 public class TravelActivity extends AppCompatActivity implements View.OnClickListener {
 
     @BindView(R.id.buttonMinutes) Button mButtonMinutes;
-
+    @BindView(R.id.minutesInput) EditText mMinutesInput;
+    @BindView(R.id.recordedTimes) TextView mRecordedTimes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +24,16 @@ public class TravelActivity extends AppCompatActivity implements View.OnClickLis
 
         ButterKnife.bind(this);
         mButtonMinutes.setOnClickListener(this);
+//        Intent intent = getIntent();
 
     }
 
     public void onClick(View v) {
         if (v == mButtonMinutes) {
+            String inputtedMinutes = mMinutesInput.getText().toString();
+            int minutes = Integer.parseInt(inputtedMinutes);
+            mRecordedTimes.setText("Your travel time(s): " + minutes);
+        } else {
 
         }
     }
