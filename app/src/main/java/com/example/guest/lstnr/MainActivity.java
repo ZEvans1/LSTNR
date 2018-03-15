7724c81ab2c85d9373651e437756297be9b0e594
Student <example@example.com>
2018-03-15 11:47:31 -0700

    Add code for buttons in MainActivity.

--- app/src/main/java/com/example/guest/lstnr/MainActivity.java
+++ app/src/main/java/com/example/guest/lstnr/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.guest.lstnr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @BindView(R.id.buttonTravel) Button mButtonTravel;
+    @BindView(R.id.buttonListen) Button mButtonListen;
+    @BindView(R.id.buttonAbout) Button mButtonAbout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        mButtonTravel.setOnClickListener(this);
+        mButtonListen.setOnClickListener(this);
+        mButtonAbout.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+
     }
 }
