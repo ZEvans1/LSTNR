6bef19b2cb4697722eeeb73295a30a09d62e245f
Student <example@example.com>
2018-03-15 14:45:17 -0700

    Create button and text field for TravelActivity.

--- app/src/main/java/com/example/guest/lstnr/TravelActivity.java
+++ app/src/main/java/com/example/guest/lstnr/TravelActivity.java
@@ -2,12 +2,30 @@ package com.example.guest.lstnr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TravelActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class TravelActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.buttonMinutes) Button mButtonMinutes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_travel);
+
+        ButterKnife.bind(this);
+        mButtonMinutes.setOnClickListener(this);
+
+    }
+
+    public void onClick(View v) {
+        if (v == mButtonMinutes) {
+
+        }
     }
 }
