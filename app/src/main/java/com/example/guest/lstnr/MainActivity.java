de51d832ec992fd9a7a576f6c24809e9204cd47d
ZEvans1 <zte.zachary@gmail.com>
2018-03-16 14:33:15 -0700

    Add in custom font (sansation).

--- app/src/main/java/com/example/guest/lstnr/MainActivity.java
+++ app/src/main/java/com/example/guest/lstnr/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.guest.lstnr;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,14 +16,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @BindView(R.id.buttonTravel) Button mButtonTravel;
 //    @BindView(R.id.buttonListen) Button mButtonListen;
     @BindView(R.id.buttonAbout) Button mButtonAbout;
+    @BindView(R.id.textView) TextView mTextView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ButterKnife.bind(this);
+
+        Typeface sansationFont = Typeface.createFromAsset(getAssets(),"fonts/Sansation-Regular.ttf");
+        mTextView.setTypeface(sansationFont);
+
+
         mButtonTravel.setOnClickListener(this);
 //        mButtonListen.setOnClickListener(this);
         mButtonAbout.setOnClickListener(this);
