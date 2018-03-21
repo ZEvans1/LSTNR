a7c0caaca03e29f0d403158bee0ea7b8a9618029
Student <example@example.com>
2018-03-21 10:47:29 -0700

    Allow array to be passed between Travel and Listen activities in both directions.

--- app/src/main/java/com/example/guest/lstnr/ListenActivity.java
+++ app/src/main/java/com/example/guest/lstnr/ListenActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -18,9 +20,20 @@ public class ListenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_listen);
         ButterKnife.bind(this);
         Intent intent = getIntent();
-        String averageString = intent.getStringExtra("averageString");
+        ArrayList<Integer> timesArray = intent.getIntegerArrayListExtra("timesArray");
+
+        Integer sum = 0;
+        for (int i = 0; i < timesArray.size(); i++){
+            sum = sum + timesArray.get(i);
+        }
+        double average = (sum/timesArray.size());
+        String averageString = Double.toString(average);
         mMinutes.setText("Your average travel time: " + averageString + " min");
 
     }
 
 }
+
+
+
+
--- app/src/main/java/com/example/guest/lstnr/TravelActivity.java
+++ app/src/main/java/com/example/guest/lstnr/TravelActivity.java
@@ -19,9 +19,8 @@ public class TravelActivity extends AppCompatActivity implements View.OnClickLis
 
     @BindView(R.id.buttonMinutes) Button mButtonMinutes;
     @BindView(R.id.minutesInput) EditText mMinutesInput;
-//    @BindView(R.id.recordedTimes) TextView mRecordedTimes;
+    @BindView(R.id.recordedTime) TextView mRecordedTime;
     @BindView(R.id.buttonTravelToListen) Button mButtonTravelToListen;
-    @BindView(R.id.minutesList) ListView mMinutesList;
 
     private ArrayList<String> times = new ArrayList<String>();
     private ArrayList<Integer> timesInt = new ArrayList<Integer>();
@@ -34,20 +33,12 @@ public class TravelActivity extends AppCompatActivity implements View.OnClickLis
         ButterKnife.bind(this);
         mButtonMinutes.setOnClickListener(this);
         mButtonTravelToListen.setOnClickListener(this);
-        TravelArrayAdapter adapter = new TravelArrayAdapter(this, android.R.layout.simple_list_item_1, times, timesInt);
-        mMinutesList.setAdapter(adapter);
 
         mButtonTravelToListen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Integer sum = 0;
-                for (int i = 0; i < timesInt.size(); i++){
-                    sum = sum + timesInt.get(i);
-                }
-                double average = (sum/timesInt.size());
-                String averageString = Double.toString(average);
                 Intent intent = new Intent(TravelActivity.this, ListenActivity.class);
-                intent.putExtra("averageString", averageString);
+                intent.putIntegerArrayListExtra("timesArray", timesInt);
                 startActivity(intent);
             }
         });
@@ -60,13 +51,7 @@ public class TravelActivity extends AppCompatActivity implements View.OnClickLis
             times.add(inputtedMinutes);
             int minutes = Integer.parseInt(inputtedMinutes);
             timesInt.add(minutes);
-//            mRecordedTimes.setText("Your travel time(s): " + minutes);
+            mRecordedTime.setText("Your travel time: " + minutes);
         }
     }
 }
-
-//                String inputtedMinutes = mMinutesInput.getText().toString();
-//                int minutes = Integer.parseInt(inputtedMinutes);
-//                Intent intent = new Intent(TravelActivity.this, ListenActivity.class);
-//                intent.putExtra("inputtedMinutes", inputtedMinutes);
-//                startActivity(intent);
